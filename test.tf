041086f505dccbde6065d4294eab2f3f2d06e8ee
sauish <saurabh12nov@gmail.com>
2024-07-25 14:37:28 +0530

    addting test changes

--- test.tf
+++ test.tf
@@ -4,12 +4,6 @@ resource "aws_s3_bucket" "example" {
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
-<<<<<<< HEAD
     region = "us-east-1"
-    Key = "new"  #Testing new branch 
-
-=======
-    Branch = "new"
->>>>>>> main
   }
 }
